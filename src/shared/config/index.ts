a5a3d6be30f6f9ecd910c059114e2bd7cdb787cf
murrchacho <murrchacho@gmail.com>
2024-07-22 20:06:35 +0500

    test

--- src/shared/config/index.ts
+++ src/shared/config/index.ts
@@ -3,5 +3,6 @@ export const ENERGY_INCREMENT = 3;
 export const ENERGY_DECREMENT = 5;
 export const ENERGY_MAX = 1500;
 
-export const API = "http://localhost:3000";
-//process.env.REACT_APP_APIKey || 
+export const API = "http://176.109.103.35:3000"
+
+//export const API = process.env.REACT_APP_APIKey || "http://localhost:3000";
